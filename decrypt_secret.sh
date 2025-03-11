39f206d4dcce5b7b2c9858f3e614d88445dcfd44
David Bradway <david.bradway@gmail.com>
2025-03-11 15:32:31 -0400

    use current working directory

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/token.json token.json.gpg
+--output token.json token.json.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/credentials.json credentials.json.gpg
+--output credentials.json credentials.json.gpg
--- testenvvar.py
+++ testenvvar.py
@@ -1,15 +1,9 @@
 import os
-# Accessing an environment variable
-home = os.environ.get("HOME")
-if home:
-    if os.path.exists(os.path.join(home, "secrets", "token.json")):
-        print("token found")
-    else:
-        print("token missing")
-    if os.path.exists(os.path.join(home, "secrets", "credentials.json")):
-        print("credentials found")
-    else:
-        print("credentials missing")
+if os.path.exists("token.json"):
+    print("token found")
 else:
-    print("home env var not found")
-
+    print("token missing")
+if os.path.exists("credentials.json"):
+    print("credentials found")
+else:
+    print("credentials missing")
