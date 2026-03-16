976d8c35d7ee37f2329b5437580d3b3bf1e14d2c
David Bradway <david.bradway@gmail.com>
2026-03-16 13:40:53 -0400

    remove decryption files

--- decrypt_secret.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output token.json token.json.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output credentials.json credentials.json.gpg
